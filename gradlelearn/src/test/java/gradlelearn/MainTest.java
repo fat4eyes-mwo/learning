3bb80ee00c854acdbf51a0abee6beda1d2939739
Neil Ongkingco <neil.ongkingco@gmail.com>
2020-11-09 12:51:14 +0800

    add learning gradle project

---
+++ gradlelearn/src/test/java/gradlelearn/MainTest.java
@@ -0,0 +1,13 @@
+package gradlelearn;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest {
+	@Test public void testMain() {
+		String[] args = {""};
+		Main.main(args);
+		assertTrue(true);
+	}
+}
